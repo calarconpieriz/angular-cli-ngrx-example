8d1b02b3bee7ab0aaaff7bc96f2b5b89f776d62b
calarconpieriz <calarconpieriz@gmail.com>
2017-04-13 15:22:52 -0300

    modifying links in app's routing file

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,6 @@ import { NotFound404Component } from './components/not-found404/component';
 export const routes: Routes = [
   { path: '', component: DashboardComponent, pathMatch: 'full' },
   { path: 'lazy', loadChildren: './components/lazy/index#LazyModule' },
-  { path: 'sync', loadChildren: './components/sync/index#SyncModule?sync=true' },
+  { path: 'sync', loadChildren: './components/sync/index#SyncModule' },
   { path: '**', component: NotFound404Component }
 ];
